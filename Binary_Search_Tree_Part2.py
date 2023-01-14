c743c0d9b3089f4bad9b5b0a14d886e58a98244b
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2023-01-14 14:20:00 +0800

    Adding the delete

--- Binary_Search_Tree_Part2.py
+++ Binary_Search_Tree_Part2.py
@@ -64,6 +64,22 @@ class BinarySearchTreeNode:
             return self.data
         return self.left.find_min()
 
+    def delete(self, val):
+        if val < self.data:
+            if self.left:
+                self.left = self.left.delete(val)
+        elif val > self.data:
+            if self.right:
+                self.right = self.right.delete(val)
+        else:
+            if self.left is None and self.right is None:
+                return None
+            elif self.left is None:
+                return self.right
+            elif self.right is None:
+                return self.left
+    
+
 def build_tree(elements):
     print("Building tree with these elements:",elements)
     root = BinarySearchTreeNode(elements[0])
